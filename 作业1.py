c53a7e18e7d55723b0167500aff2b906886d9a9c
王阿瑾 <90880781+319905147@users.noreply.github.com>
2021-11-30 15:43:07 +0800

    Add files via upload

--- 作业1.py
+++ 作业1.py
@@ -1,23 +1,24 @@
-# 一个列表若有20个整数组成,要求:将偶数放在前面,奇数放在后面,并输出该列表,(初始20个数据由用户任意输入)
+# 1 眼看要过年了,深夜食堂经营的不错,你打算给员工发奖金犒劳一下,请你定义函数,当输入员工姓名和工作时长两个参数时,即可大印出该员工获得的奖金
+# 发奖金的要求要求如下:
+# 工作时长小于6 月，发国定奖金500元
+# 时长在 6月到一年 （包含一年）每月奖金 120元
+# 超过一年 每月奖金 180元
+name = input('请输入员工的姓名 : ')
+month = eval(input('请输入工作的时长 : '))
 
 
-my_list2 = []
-my_list1 = []
-my_list = input("请输入20个整数，两数之间用空格隔开：").split(" ")
-try:
-    if len(my_list) == 20:
-        for i in range(len(my_list)):
-            t = int(my_list[i])
-            my_list[i] = t
-        for j in my_list:
-            if j % 2 == 0:
-                my_list1.append(j)
-            else:
-                my_list2.append(j)
-        my_list1.extend(my_list2)
-        print(my_list1)
+def func(name, month):
+    if 0 < month < 6:
+        print(name)
+        print('奖金为 500')
+    elif 6 <= month <= 12:
+        print(name)
+        print('奖金为 %d 元' % (month * 120))
+    elif 12 <= month:
+        print(name)
+        print('奖金为 %d 元' % (month * 180))
     else:
-        print('你输入的数字不满足20 个整数')
-except ValueError as f:
-    print('你的输入里有字符,不是纯数字,请重新输入 ')
-    print(f)  # 打印字符异常信息
+        pass
+
+
+func(name, month)
